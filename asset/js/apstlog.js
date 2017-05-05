4a3d83283f6cd0d452313379f85b1ff435e757b3
tsubaki <tsubaki@moon>
2017-05-05 21:08:45 +0900

    Refactor "" -> ''

--- asset/js/apstlog.js
+++ asset/js/apstlog.js
@@ -16,43 +16,45 @@ function init() {
   ga('send', 'pageview');
 
   // option
-  d3.json(base_url + "/review?output=json", function(error, data) {
+  d3.json(base_url + '/review?output=json', function(error, data) {
     for (var id of data) {
-      d3.select("select")
-        .append("option")
-        .attr("value", id)
+      d3.select('select')
+        .append('option')
+        .attr('value', id)
         .html(id)
     }
   })
 }
 
 function clear() {
-  d3.selectAll("div").remove()
-  d3.selectAll("svg").remove()
-  d3.selectAll("h4").remove()
-  d3.selectAll("br").remove()
+  d3.selectAll('div').remove()
+  d3.selectAll('svg').remove()
+  d3.selectAll('h4').remove()
+  d3.selectAll('br').remove()
 
-  d3.select("body")
-    .append("div")
-    .attr("id", "tooltip")
-    .attr("class", "btn btn-default")
-    .attr("data-toggle", "tooltip")
-    .attr("data-placement", "top")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
+/*
+  d3.select('body')
+    .append('div')
+    .attr('id', 'tooltip')
+    .attr('class', 'btn btn-default')
+    .attr('data-toggle', 'tooltip')
+    .attr('data-placement', 'top')
+    .style('position', 'absolute')
+    .style('z-index', '10')
+    .style('visibility', 'hidden')
+*/
 
 }
 
 function review_all() {
   clear()
 
-  d3.json(base_url + "/review?output=json", function(error, data) {
+  d3.json(base_url + '/review?output=json', function(error, data) {
     for (var id of data) {
-      d3.select("body")
-        .append("div")
-        .attr("class", "container")
-        .attr("id", "id" + id)
+      d3.select('body')
+        .append('div')
+        .attr('class', 'container')
+        .attr('id', 'id' + id)
     }
     for (var id of data) {
       review(id)
@@ -61,64 +63,64 @@ function review_all() {
 }
 
 function review(id) {
-  var url = base_url + "/review/search?limit=200&id=" + id
-  d3.json(url + "&output=json", function(error, data) {
+  var url = base_url + '/review/search?limit=200&id=' + id
+  d3.json(url + '&output=json', function(error, data) {
 
-    var div = d3.select("#id" + id)
-    div.append("h4").html("<a href=\"" + url + "\">" + id + "</a>")
-    div.append("br")
+    var div = d3.select('#id' + id)
+    div.append('h4').html('<a href=\'' + url + '\'>' + id + '</a>')
+    div.append('br')
 
-    var svg = div.append("svg")
-      .attr("width", 1100)
-      .attr("height", 200)
+    var svg = div.append('svg')
+      .attr('width', 1100)
+      .attr('height', 200)
 
-    svg.selectAll("circle")
+    svg.selectAll('circle')
       .data(data)
       .enter()
-      .append("circle")
-      .attr("cx", function(d, i) {
+      .append('circle')
+      .attr('cx', function(d, i) {
         return 30 + (i * 15)
       })
-      .attr("cy", 100)
-      .attr("r", function(d) {
+      .attr('cy', 100)
+      .attr('r', function(d) {
         return d.Content.length / 5
       })
-      .attr("fill", function(d) {
+      .attr('fill', function(d) {
         switch (d.Rating) {
-          case "1":
-            return "red"
-          case "2":
-            return "orange"
-          case "3":
-            return "black"
-          case "4":
-            return "blue"
-          case "5":
-            return "green"
+          case '1':
+            return 'red'
+          case '2':
+            return 'orange'
+          case '3':
+            return 'black'
+          case '4':
+            return 'blue'
+          case '5':
+            return 'green'
         }
 
       })
-      .style("opacity", 0.2)
-      .append("title")
+      .style('opacity', 0.2)
+      .append('title')
       .text(function(d) {
         return d.Content
       })
 
     /*
-          .on("mouseover", function(d) {
-            return d3.select("#tooltip")
-              .style("visibility", "visible")
-            //.attr("title", d.Content)
+          .on('mouseover', function(d) {
+            return d3.select('#tooltip')
+              .style('visibility', 'visible')
+            //.attr('title', d.Content)
             //.text(d.Content)
           })
-          .on("mousemove", function() {
-            return d3.select("#tooltip")
-              .style("top", (event.pageY - 10) + "px")
-              .style("left", (event.pageX + 10) + "px");
+          .on('mousemove', function() {
+            return d3.select('#tooltip')
+              .style('top', (event.pageY - 10) + 'px')
+              .style('left', (event.pageX + 10) + 'px');
           })
-          .on("mouseout", function() {
-            return d3.select("#tooltip")
-              .style("visibility", "hidden");
+          .on('mouseout', function() {
+            return d3.select('#tooltip')
+              .style('visibility', 'hidden');
           })
           */
   })
@@ -127,14 +129,14 @@ function review(id) {
 function onChange_review() {
   clear()
   var id = document.form.data.value
-  if (id == "all") {
+  if (id == 'all') {
     review_all()
     return
   }
 
-  d3.select("body")
-    .append("div")
-    .attr("class", "container")
-    .attr("id", "id" + id)
+  d3.select('body')
+    .append('div')
+    .attr('class', 'container')
+    .attr('id', 'id' + id)
   review(id)
 }
