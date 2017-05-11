cf3ae6eda555189df165067fbaf2f4a314e7652d
tsubaki <tsubaki@moon>
2017-05-11 20:30:21 +0900

    Update

--- asset/js/apstlog.js
+++ asset/js/apstlog.js
@@ -1,19 +1,4 @@
 function init() {
-  // google analytics
-  (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-  ga('create', tracking_code, 'auto');
-  ga('send', 'pageview');
-
   // option
   d3.json(base_url + '/review?output=json', function(error, data) {
     for (var id of data) {
